fc3e1f92fd7e56fc111dec835f36a4fcee8d104d
Elwyn Chow <elwyn5150@gmail.com>
2021-09-27 02:17:56 -0400

    Added code so that it can request just one specific city. Added param information

--- weather.php
+++ weather.php
@@ -3,7 +3,12 @@
 //-----------------------------------------------------------------------------
 /**
  * Get array of max temperatures of most recent 7 days for a given city
+ * @param database handler	$db		database handler
+ * @param string		$city		name of the city
+ * @param string		$max_or_min	"max" or "min" temperature
+ * @return array				array of temperatures
  */
+
 function get_temperatures($db, $city, $days, $max_or_min) {
 	$temperatures = array();
 
@@ -26,9 +31,31 @@ EOSQL;
 	}
 	return $temperatures;
 }
+
+//-----------------------------------------------------------------------------
+/**
+ * Append city's details to data array
+ * @param database handler	$db	database handler
+ * @param associative array	$data	associative array of data
+ * @param string		$city	name of city
+ */
+
+function append_city_data($db, &$data, $city) {
+	$data[] = array(
+		"name" => $city . " (max)",
+		"data" => get_temperatures($db, $city, 7, "max")
+	);
+	$data[] = array(
+		"name" => $city . " (min)",
+		"data" => get_temperatures($db, $city, 7, "min")
+	);
+}
 //-----------------------------------------------------------------------------
 
 $ini_array = parse_ini_file("weather.ini");
+$city = $_GET["city"];
+
+//TODO: Added some validation/sanitization of the variable $city
 $data = array();
 
 header('Content-Type: application/json; charset=utf-8');
@@ -40,19 +67,18 @@ $db = new mysqli(
 		$ini_array["database"]
 );
 
-$result = $db->query("select distinct(city) from weather_reports");
+if ( isset($city) ) {
+	append_city_data($db, $data, $city);
+} else {
+	// No city is set: fetch records for all cities
+	$result = $db->query("select distinct(city) from weather_reports");
+	
+	while($row = $result->fetch_row()) {
+		$city = $row[0];
 
-while($row = $result->fetch_row()) {
-	$city = $row[0];
+		append_city_data($db, $data, $city);
+	}
 
-	$data[] = array(
-		"name" => $city . " (max)",
-		"data" => get_temperatures($db, $city, 7, "max")
-	);
-	$data[] = array(
-		"name" => $city . " (min)",
-		"data" => get_temperatures($db, $city, 7, "min")
-	);
 }
 
 echo json_encode($data);
